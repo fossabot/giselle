38c27a3fe1fe6f4d18361e80b3a375dd9bbec0b6
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-10-18 18:14:32 +0900

    Use `registerOTel()` to simplify client configuration

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,25 @@
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import * as Sentry from "@sentry/nextjs";
+import { registerOTel } from "@vercel/otel";
 
 export async function register() {
+	registerOTel({
+		serviceName: "giselle",
+		attributes: {
+			environment: process.env.VERCEL_ENV || "not-set",
+		},
+		metricReader: new PeriodicExportingMetricReader({
+			exporter: new OTLPMetricExporter({
+				url: "https://ingest.us.signoz.cloud:443/v1/metrics",
+				headers: {
+					"signoz-access-token": process.env.SIGNOZ_INGESTION_TOKEN,
+				},
+			}),
+			exportIntervalMillis: 10000,
+		}),
+	});
+
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		await import("./sentry.server.config");
 	}
