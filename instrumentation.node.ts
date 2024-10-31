d59da7cd4fa456f375f0725f672ec9174f299b96
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-10-31 12:47:02 +0900

    Use registerOTel()

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,27 +1,71 @@
-import { metrics } from "@opentelemetry/api";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
-import { Resource } from "@opentelemetry/resources";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import {
+	BatchLogRecordProcessor,
+	ConsoleLogRecordExporter,
+	LoggerProvider,
+} from "@opentelemetry/sdk-logs";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import {
+	ConsoleSpanExporter,
+	SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { registerOTel } from "@vercel/otel";
 
-// Configure OTLP metrics exporter
-const otlpMetricsExporter = new OTLPMetricExporter({
+const headers = {
+	"signoz-access-token": process.env.SIGNOZ_INGESTION_TOKEN,
+};
+
+// Metric Exporter
+const metricExporter = new OTLPMetricExporter({
 	url: "https://ingest.us.signoz.cloud:443/v1/metrics",
-	headers: {
-		"signoz-access-token": process.env.SIGNOZ_INGESTION_TOKEN,
-	},
+	headers,
 });
 
-// Define resource with service name
-const sdk = new NodeSDK({
-	resource: new Resource({
-		[SEMRESATTRS_SERVICE_NAME]: "serverside",
-		environment: process.env.NEXT_PUBLIC_VERCEL_ENV || "not-set",
-	}),
-	metricReader: new PeriodicExportingMetricReader({
-		exporter: otlpMetricsExporter,
-		exportIntervalMillis: 10000,
-	}),
+export const metricReader = new PeriodicExportingMetricReader({
+	exporter: metricExporter,
+	exportIntervalMillis: 10000,
 });
-sdk.start();
+
+// Trace Exporter
+const traceExporter = new OTLPTraceExporter({
+	url: "https://ingest.us.signoz.cloud:443/v1/traces",
+	headers,
+});
+
+const spanProcessor = new BatchSpanProcessor(traceExporter);
+
+// For debug purposes, you can also use SimpleSpanProcessor with ConsoleSpanExporter
+const debugSpanProcessor = new SimpleSpanProcessor(new ConsoleSpanExporter());
+
+// Log Exporter
+const logExporter = new OTLPLogExporter({
+	url: "https://ingest.us.signoz.cloud:443/v1/logs",
+	headers,
+});
+
+export const loggerProvider = new LoggerProvider();
+loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
+
+// Only add ConsoleLogRecordExporter in development
+if (process.env.NODE_ENV !== "production") {
+	loggerProvider.addLogRecordProcessor(
+		new BatchLogRecordProcessor(new ConsoleLogRecordExporter()),
+	);
+}
+
+const logRecordProcessor = new BatchLogRecordProcessor(logExporter);
+loggerProvider.addLogRecordProcessor(logRecordProcessor);
+
+registerOTel({
+	serviceName: "giselle",
+	metricReader: metricReader,
+	spanProcessors: [spanProcessor, debugSpanProcessor],
+	traceExporter: traceExporter,
+	logRecordProcessor: logRecordProcessor,
+});
+
+export const logger = loggerProvider.getLogger("giselle");
+console.log("-- OTEL registered with metrics, traces, and logs --");
