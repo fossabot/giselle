371ce40c4c52d445379de2dff64d3607bf93529b
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-11-06 21:21:08 +0900

    test

---
+++ app/dev/langfuse/route.ts
@@ -0,0 +1,17 @@
+import { openai } from "@ai-sdk/openai";
+import { createId } from "@paralleldrive/cuid2";
+import { generateText } from "ai";
+import Langfuse from "langfuse";
+
+export async function POST() {
+	// const lf = new Langfuse();
+	// const trace = lf.trace({
+	// 	id: `toyama-${createId()}`,
+	// });
+	const { text } = await generateText({
+		model: openai("gpt-4o-mini"),
+		prompt: "Hello, World!",
+		experimental_telemetry: { isEnabled: true },
+	});
+	return Response.json({ message: text });
+}
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
+import { registerOTel } from "@vercel/otel";
+import { LangfuseExporter } from "langfuse-vercel";
 
 export async function register() {
 	if (process.env.NEXT_RUNTIME === "nodejs") {
@@ -8,6 +10,11 @@ export async function register() {
 	if (process.env.NEXT_RUNTIME === "edge") {
 		await import("./sentry.edge.config");
 	}
+
+	registerOTel({
+		serviceName: "langfuse-vercel-ai-nextjs-example",
+		traceExporter: new LangfuseExporter({ debug: true }),
+	});
 }
 
 export const onRequestError = Sentry.captureRequestError;
