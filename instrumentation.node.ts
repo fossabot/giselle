0bb198ad32ef223cd69f85a5185404c65d1a9cf1
Akira Hayashi <akira.hayashi@route06.co.jp>
2024-11-08 13:16:22 +0900

    Setup OTEL in `lib/opentelemetry`

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,49 +1,9 @@
-import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
-import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import {
-	BatchLogRecordProcessor,
-	ConsoleLogRecordExporter,
-	LoggerProvider,
-} from "@opentelemetry/sdk-logs";
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { logRecordProcessor, metricReader } from "@/lib/opentelemetry";
 import { registerOTel } from "@vercel/otel";
-
-console.log("Initializing OTEL SDK------------------------");
-
-const headers = {
-	"signoz-access-token": process.env.SIGNOZ_INGESTION_TOKEN,
-};
-
-// Metric Exporter
-const metricExporter = new OTLPMetricExporter({
-	url: "https://ingest.us.signoz.cloud:443/v1/metrics",
-	headers,
-});
-
-const metricReader = new PeriodicExportingMetricReader({
-	exporter: metricExporter,
-	exportIntervalMillis: 14000,
-	exportTimeoutMillis: 13000,
-});
-
-// Log Exporter
-const logExporter = new OTLPLogExporter({
-	url: "https://ingest.us.signoz.cloud:443/v1/logs",
-	headers,
-});
-
-const loggerProvider = new LoggerProvider();
-loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
-loggerProvider.addLogRecordProcessor(
-	new BatchLogRecordProcessor(new ConsoleLogRecordExporter()),
-);
 
 registerOTel({
 	serviceName: "giselle",
 	metricReader,
-	logRecordProcessor: new BatchLogRecordProcessor(logExporter),
+	logRecordProcessor,
 });
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 console.log("-- OTEL registered with metrics, traces, and logs --");
---
+++ lib/opentelemetry/setup.ts
@@ -0,0 +1,44 @@
+import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import {
+	BatchLogRecordProcessor,
+	ConsoleLogRecordExporter,
+	LoggerProvider,
+} from "@opentelemetry/sdk-logs";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+
+const headers = {
+	"signoz-access-token": process.env.SIGNOZ_INGESTION_TOKEN,
+};
+
+// Metrics
+const metricExporter = new OTLPMetricExporter({
+	url: "https://ingest.us.signoz.cloud:443/v1/metrics",
+	headers,
+});
+
+export const metricReader = new PeriodicExportingMetricReader({
+	exporter: metricExporter,
+	exportIntervalMillis: 14000,
+	exportTimeoutMillis: 13000,
+});
+
+// Logs
+const logExporter = new OTLPLogExporter({
+	url: "https://ingest.us.signoz.cloud:443/v1/logs",
+	headers,
+});
+
+export const logRecordProcessor = new BatchLogRecordProcessor(logExporter);
+
+const loggerProvider = new LoggerProvider();
+loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
+loggerProvider.addLogRecordProcessor(
+	new BatchLogRecordProcessor(new ConsoleLogRecordExporter()),
+);
+
+export const logger = loggerProvider.getLogger("giselles");
+
+// To debug OTEL SDK itself
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
